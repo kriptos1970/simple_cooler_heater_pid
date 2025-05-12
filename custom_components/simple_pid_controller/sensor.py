0ccb3ee3a55ebe2000e6d319f4b87aa9577262c5
bvweerd <bastiaan.vanweerd@aimvalley.com>
2025-05-12 08:57:40 +0200

    Update sensor.py

--- custom_components/simple_pid_controller/sensor.py
+++ custom_components/simple_pid_controller/sensor.py
@@ -131,7 +131,7 @@ class PIDOutputSensor(CoordinatorEntity[PIDDataCoordinator], SensorEntity):
         super().__init__(coordinator)
         self._attr_unique_id = f"{entry.entry_id}_pid_output"
         self._attr_name = f"PID Output"
-        self._attr_has_entity_name = False
+        self._attr_has_entity_name = True
         self._attr_native_unit_of_measurement = "%"
 
         # Device-info
