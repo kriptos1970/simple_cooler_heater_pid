f526259f1ee34f215b48a4cda3a1e45c364b0e1a
bvweerd <b.van.weerd@gmail.com>
2025-05-18 09:46:22 +0200

    fix default name

--- custom_components/simple_pid_controller/config_flow.py
+++ custom_components/simple_pid_controller/config_flow.py
@@ -16,7 +16,7 @@ from homeassistant.config_entries import (
 from homeassistant.core import callback
 from homeassistant.helpers.selector import selector
 
-from .const import DOMAIN, CONF_NAME, CONF_SENSOR_ENTITY_ID
+from .const import DOMAIN, CONF_NAME, DEFAULT_NAME, CONF_SENSOR_ENTITY_ID
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -52,7 +52,7 @@ class PIDControllerFlowHandler(ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {
                     vol.Required(
-                        CONF_NAME, default=self.hass.config.location_name
+                        CONF_NAME, default=DEFAULT_NAME
                     ): str,
                     vol.Required(CONF_SENSOR_ENTITY_ID): selector(
                         {"entity": {"domain": "sensor"}}
--- custom_components/simple_pid_controller/const.py
+++ custom_components/simple_pid_controller/const.py
@@ -2,4 +2,5 @@
 
 DOMAIN = "simple_pid_controller"
 CONF_NAME = "name"
+DEFAULT_NAME = "sPID-x"
 CONF_SENSOR_ENTITY_ID = "sensor_entity_id"
